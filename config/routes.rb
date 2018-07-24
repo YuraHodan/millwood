7b870f8ad26bb7b0469a0a4914651174f0c0e042
YuraHodan <yura.hodan@gmail.com>
2018-07-24 12:44:51 +0300

    bags 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   controller :pages do
     get "about-us", action: "about_us"
     # get "contact-us", action: "contact_us"
+    get "company", action: "company"
   end
 
   match "*url", to: "application#render_not_found", via: [:get, :post, :path, :put, :update, :delete]
