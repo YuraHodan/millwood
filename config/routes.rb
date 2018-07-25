b36be969968ad51d147d763dd6e47aec34a6fc32
YuraHodan <yura.hodan@gmail.com>
2018-07-25 17:30:49 +0300

    one products

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     # get "contact-us", action: "contact_us"
     get "company", action: "company"
     get "producters", action: "producters"
+    get "whoweare", action: "whoweare"
+    get "locations", action: "locations"
+    get "productkvh", action: "productkvh"
+    get "oneproducterssawn", action: "oneproducterssawn"
   end
 
   match "*url", to: "application#render_not_found", via: [:get, :post, :path, :put, :update, :delete]
