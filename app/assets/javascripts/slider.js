6d91c486ff3121d5c0e9c81dc500ef61668a8cd9
YuraHodan <yura.hodan@gmail.com>
2018-08-08 12:10:41 +0300

    slider end form end

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -26,23 +26,40 @@ $('.goode2-slider-nav').slick({
 $('.goode-slider-for').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
- arrows: true,
- fade: true,
+ // arrows: true,
+ // fade: true,
  asNavFor: '.goode-slider-nav'
 });
 $('.goode-slider-nav').slick({
- slidesToShow: 1,
- slidesToScroll: 1,
+ slidesToShow: 3,
+ slidesToScroll: 3,
  asNavFor: '.goode-slider-for',
- dots: true,
- infinite: true,
- speed: 500,
- fade: true,
- autoplay: false,
- autoplaySpeed: 2000,
- nextArrow: false,
- prevArrow: false,
- cssEase: 'linear',
- nextArrow: $('.mainlidet2').find('.next-btn'),
- prevArrow: $('.mainlidet2').find('.prev-btn')
+ // dots: true,
+ // infinite: true,
+ // speed: 500,
+ // fade: true,
+ // autoplay: false,
+ // autoplaySpeed: 2000,
+ // nextArrow: false,
+ // prevArrow: false,
+ // cssEase: 'linear',
+ // nextArrow: $('.mainlidet2').find('.next-btn'),
+ // prevArrow: $('.mainlidet2').find('.prev-btn'),
+ responsive:[
+   {
+   breakpoint: 376,
+   settings:{
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     dots: true,
+     infinite: true,
+     speed: 500,
+     fade: true,
+     autoplay: false,
+     autoplaySpeed: 2000,
+     nextArrow: false,
+     prevArrow: false,
+     cssEase: 'linear'
+   }
+ }]
 });
