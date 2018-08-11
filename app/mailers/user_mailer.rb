e3ee1019382033bf3991a8b62c9e5c1c2b7591c3
Viktor <vodarchenko@gmail.com>
2018-08-11 20:19:22 +0300

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ApplicationMailer
  default from: ENV["smtp_gmail_user_name"]
 
  def consultation_email(consultation)
-   @message = message
-   mail(to: "eduard.m@voroninstudio.eu", subject: 'Питання')
+   @message = consultation
+   mail(to: "vodarchenko@gmail.com", subject: 'Питання')
  end
 
 end
