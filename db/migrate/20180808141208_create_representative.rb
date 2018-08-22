0fab8d1dcbcc1f10fbee51ad4861047da78ef65f
edwardedd <eddmos86@gmail.com>
2018-08-22 15:34:10 +0300

    12

--- db/migrate/20180808141208_create_representative.rb
+++ db/migrate/20180808141208_create_representative.rb
@@ -1,12 +1,12 @@
 class CreateRepresentative < ActiveRecord::Migration
-  def change
-    create_table :representatives do |t|
-    	t.string :title
-    	t.text   :description    	
-    	t.text   :address
-    	t.text   :contact_number
-    	t.string :image
-    	t.string :right_title
+    def change
+        create_table :representatives do |t|
+            t.string :title
+            t.text   :description       
+            t.text   :address
+            t.text   :contact_number
+            t.string :image
+            t.string :right_title
+        end
     end
-  end
 end
